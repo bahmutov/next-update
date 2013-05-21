b940dd496256eaf15ebbfededababf62a2c61cc1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-05-20 23:00:36 -0400

    prefer global installation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var package = require('./package.json');
 
 var info = package.name + ' - ' + package.description + '\n' +
     '  version: ' + package.version + '\n' +
-    '  author: ' + package.author;
+    '  author: ' + JSON.stringify(package.author);
 
 var program = require('optimist')
     .usage(info)
