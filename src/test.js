5dede730725e5a1f2cb385f79727717f47a34787
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-05-22 00:17:36 -0400

    spawn for npm test command

--- src/test.js
+++ src/test.js
@@ -1,7 +1,30 @@
 var check = require('check-type');
+var spawn = require('child_process').spawn;
 
 function test(callback) {
     check.verifyFunction(callback, 'expected callback function');
+
+    var npm = spawn('npm', ['test']);
+    var testOutput = '';
+    var testErrors = '';
+
+    npm.stdout.setEncoding('utf-8');
+    npm.stdout.on('data', function (data) {
+        testOutput += data.trim();
+    });
+
+    npm.stderr.setEncoding('utf-8');
+    npm.stderr.on('data', function (data) {
+        testErrors += data.trim();
+    });
+
+    npm.on('exit', function (code) {
+        if (code) {
+            console.error('npm test returned', code);
+            console.error('test errors:\n' + testErrors);
+        }
+        callback(null, code);
+    });
 }
 
 module.exports = test;
