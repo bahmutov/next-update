b55a6774d31dd4a7e9e3cb3d1ddc5e97dea49885
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-05-25 00:20:52 -0400

    listing dependencies, fixes #20

--- src/test/registry.js
+++ src/test/registry.js
@@ -34,14 +34,14 @@ gt.async('fetch gt later versions', function () {
     }).fail(onError).fin(gt.start);
 }, 30000);
 
-gt.async('fetch async later versions', function () {
+gt.async('fetch module later versions', function () {
     gt.func(fetchVersions);
     gt.arity(fetchVersions, 1);
-    var promise = fetchVersions(['async', '0.2.0']);
+    var promise = fetchVersions(['lodash', '0.7.0']);
     gt.func(promise.then, 'return object has then method');
     promise.then(function (results) {
         gt.object(results, 'returns an object');
-        gt.equal(results.name, 'async', 'correct name returned');
+        gt.equal(results.name, 'lodash', 'correct name returned');
         gt.array(results.versions, 'has versions array');
         gt.ok(results.versions.length > 5, 'a few versions');
     }).fail(onError).fin(gt.start);
@@ -56,29 +56,29 @@ gt.test('next versions basics', function () {
     gt.arity(nextVersions, 2);
 });
 
-gt.async('fetch gt, async versions', function () {
-    var promise = nextVersions([['gt', '0.5.0'], ['async', '0.2.0']]);
+gt.async('fetch gt, optimist versions', function () {
+    var promise = nextVersions([['gt', '0.5.0'], ['lodash', '1.0.0']]);
     gt.func(promise.then, 'return object has then method');
     promise.then(function (results) {
         gt.array(results);
         gt.equal(results.length, 2, 'two modules');
         gt.equal(results[0].name, 'gt');
-        gt.equal(results[1].name, 'async');
-        gt.equal(results[0].vesions[0], '0.5.1');
-        gt.equal(results[1].vesions[0], '0.2.1');
+        gt.equal(results[1].name, 'lodash');
+        gt.equal(results[0].versions[0], '0.5.1');
+        gt.equal(results[1].versions[0], '1.0.1');
     }).fail(onError).fin(gt.start);
 }, 30000);
 
 gt.async('fetch latest version', function () {
     var onlyLatest = true;
-    var promise = nextVersions([['gt', '0.5.0'], ['async', '0.2.0']], onlyLatest);
+    var promise = nextVersions([['gt', '0.5.0'], ['lodash', '1.0.0']], onlyLatest);
     gt.func(promise.then, 'return object has then method');
     promise.then(function (results) {
         gt.array(results);
         gt.equal(results.length, 2, 'two modules');
         gt.equal(results[0].name, 'gt');
-        gt.equal(results[1].name, 'async');
-        gt.equal(results[0].vesions.length, 1);
-        gt.equal(results[1].vesions.length, 1);
+        gt.equal(results[1].name, 'lodash');
+        gt.equal(results[0].versions.length, 1);
+        gt.equal(results[1].versions.length, 1);
     }).fail(onError).fin(gt.start);
 }, 30000);
