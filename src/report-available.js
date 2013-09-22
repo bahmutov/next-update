0622f5be8309cf6230efd6be4113218c0dbd0900
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-09-22 00:55:00 -0400

    fixed jshint errors

--- src/report-available.js
+++ src/report-available.js
@@ -1,24 +1,23 @@
 var check = require('check-types');
 
-// prints available module versions
 function report(available) {
-  check.verifyArray(available, 'expect an array of info objects');
-  if (!available.length) {
-    console.log('nothing new is available');
-    return;
-  }
-
-  console.log('\navailable versions');
-
-  available.forEach(function (info) {
-    check.verifyString(info.name, 'missing module name ' + info);
-    check.verifyArray(info.versions, 'missing module versions ' + info);
-    var sep = ', ';
-    if (info.versions.length > 5) {
-      sep = '\n  ';
+    check.verifyArray(available, 'expect an array of info objects');
+    if (!available.length) {
+        console.log('nothing new is available');
+        return;
     }
-    console.log(info.name, ': ', info.versions.join(sep));
-  });
+
+    console.log('\navailable versions');
+
+    available.forEach(function (info) {
+        check.verifyString(info.name, 'missing module name ' + info);
+        check.verifyArray(info.versions, 'missing module versions ' + info);
+        var sep = ', ';
+        if (info.versions.length > 5) {
+            sep = '\n  ';
+        }
+        console.log(info.name, ': ', info.versions.join(sep));
+    });
 }
 
 module.exports = report;
--- src/report-install-command.js
+++ src/report-install-command.js
@@ -1,41 +1,36 @@
 var check = require('check-types');
 
-// returns the string command
 function installCommand(updates) {
-  check.verifyArray(updates, 'expected array of updates');
-  if (!updates.length) {
-    return;
-  }
-
-  // todo: use correct dep type, not just all --save
-  var cmd = 'npm install --save';
-  updates.forEach(function (moduleVersions) {
-    var latestWorkingVersion = getLatestWorkingVersion(moduleVersions);
-    if (latestWorkingVersion) {
-      cmd += ' ' + latestWorkingVersion.name + '@' + latestWorkingVersion.version;
+    check.verifyArray(updates, 'expected array of updates');
+    if (!updates.length) {
+        return;
     }
-  });
 
-  if ((/--save$/).test(cmd)) {
-    // nothing works
-    return;
-  }
+    var cmd = 'npm install --save';
+    updates.forEach(function (moduleVersions) {
+        var latestWorkingVersion = getLatestWorkingVersion(moduleVersions);
+        if (latestWorkingVersion) {
+            cmd += ' ' + latestWorkingVersion.name + '@' + latestWorkingVersion.version;
+        }
+    });
 
-  return cmd;
+    if ((/--save$/).test(cmd)) {
+        return;
+    }
+
+    return cmd;
 }
 
 function getLatestWorkingVersion(versions) {
-  check.verifyArray(versions, 'expected array of versions');
-  // assume versions are sorted, latest is last
+    check.verifyArray(versions, 'expected array of versions');
+    var working;
+    versions.forEach(function (info) {
+        if (info.works) {
+            working = info;
+        }
+    });
 
-  var working;
-  versions.forEach(function (info) {
-    if (info.works) {
-      working = info;
-    }
-  });
-
-  return working;
+    return working;
 }
 
 module.exports = installCommand;
