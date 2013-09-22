24a200f410338e3f7f6467d41974f4ba22d37e6f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-09-22 16:12:02 -0400

    printing install commands by type

--- src/report-install-command.js
+++ src/report-install-command.js
@@ -1,4 +1,50 @@
 var check = require('check-types');
+var _ = require('lodash');
+
+function loadPackage() {
+    var fs = require('fs');
+    var txt = fs.readFileSync('./package.json', 'utf-8');
+    var pkg = JSON.parse(txt);
+    return pkg;
+}
+
+function saveOption(type) {
+    var saveCommands = {
+        dependencies: '--save',
+        devDependencies: '--save-dev',
+        peerDependencies: '--save-peer'
+    }
+    return saveCommands[type];
+}
+
+function splitByType(updates, pkg) {
+    check.verifyArray(updates, 'expected array of updates');
+    check.verifyObject(pkg, 'expected package object');
+
+    var result = {
+        dependencies: [],
+        devDependencies: [],
+        peerDependencies: []
+    }
+
+    updates.forEach(function (moduleList) {
+        if (!moduleList.length) {
+            return;
+        }
+        var moduleName = moduleList[0].name;
+        check.verifyString(moduleName, 'missing module name');
+        if (pkg.dependencies && pkg.dependencies[moduleName]) {
+            result.dependencies.push(moduleList);
+        } else if (pkg.devDependencies && pkg.devDependencies[moduleName]) {
+            result.devDependencies.push(moduleList);
+        } else if (pkg.peerDependencies && pkg.peerDependencies[moduleName]) {
+            result.peerDependencies.push(moduleList);
+        } else {
+            throw new Error('Could not find dependency for ' + moduleName);
+        }
+    });
+    return result;
+}
 
 function installCommand(updates) {
     check.verifyArray(updates, 'expected array of updates');
@@ -6,7 +52,44 @@ function installCommand(updates) {
         return;
     }
 
-    var cmd = 'npm install --save';
+    var pkg = loadPackage();
+    console.assert(pkg, 'could not load package.json');
+
+    var updatesByDependencyType = splitByType(updates, pkg);
+    console.assert(updatesByDependencyType, 'could not split updates by type');
+    console.log('dep by type');
+    console.log(JSON.stringify(updatesByDependencyType, null, 2));
+
+    var cmd = '';
+    var depCmd = installCommandType(updatesByDependencyType.dependencies, 'dependencies');
+    var devCmd = installCommandType(updatesByDependencyType.devDependencies, 'devDependencies');
+    var peerCmd = installCommandType(updatesByDependencyType.peerDependencies, 'peerDependencies');
+    if (depCmd) {
+        cmd += depCmd + '\n';
+    }
+    if (devCmd) {
+        cmd += devCmd + '\n';
+    }
+    if (peerCmd) {
+        cmd += peerCmd + '\n';
+    }
+    if (cmd) {
+        return cmd;
+    }
+}
+
+function installCommandType(updates, type) {
+    check.verifyArray(updates, 'expected array of updates');
+    if (!updates.length) {
+        return;
+    }
+    check.verifyString(type, 'missing type');
+
+    var saveCommand = saveOption(type);
+    if (!saveCommand) {
+        throw new Error('invalid dependency type ' + type);
+    }
+    var originalCmd = cmd = 'npm install ' + saveCommand;
     updates.forEach(function (moduleVersions) {
         var latestWorkingVersion = getLatestWorkingVersion(moduleVersions);
         if (latestWorkingVersion) {
@@ -14,7 +97,7 @@ function installCommand(updates) {
         }
     });
 
-    if ((/--save$/).test(cmd)) {
+    if (originalCmd === cmd) {
         return;
     }
 
@@ -25,6 +108,7 @@ function getLatestWorkingVersion(versions) {
     check.verifyArray(versions, 'expected array of versions');
     var working;
     versions.forEach(function (info) {
+        check.verifyString(info.name, 'missing package name ' + info);
         if (info.works) {
             working = info;
         }
--- src/test/report-install-command.coffee
+++ src/test/report-install-command.coffee
@@ -1,6 +1,38 @@
-gt.module 'report-install-command'
-
 report = require '../report-install-command'
+mockery = require 'mockery'
+
+mockPackageJson = """
+{
+  "name": "next-update",
+  "version": "0.0.4",
+  "dependencies": {
+    "optimist": "~0.6.0",
+    "lodash": "~2.0.0",
+    "check-types": "~0.6.4"
+  },
+  "devDependencies": {
+    "grunt": "~0.4.1",
+    "grunt-contrib-jshint": "~0.6.4",
+    "grunt-bump": "0.0.11",
+    "gt": "~0.8.10"
+  },
+  "peerDependencies": {
+    "foo": "0.0.1"
+  }
+}
+"""
+
+fsMock =
+  readFileSync: (name, encoding) ->
+    console.assert name == './package.json', 'invalid name ' + name
+    mockPackageJson
+
+mockery.registerMock 'fs', fsMock
+
+gt.module 'report-install-command',
+  setup: -> mockery.enable { useCleanCache : true }
+  teardown: -> mockery.disable()
+
 
 gt.test 'basics', ->
   gt.arity report, 1
@@ -34,6 +66,36 @@ gt.test 'single working', ->
   ]
   gt.ok /foo@0.3.0/.test(cmd), 'latest version'
 
+gt.test 'peer dependency command', ->
+  cmd = report [
+    [
+      name: 'foo'
+      works: true
+      version: '0.3.0'
+    ]
+  ]
+  gt.ok /--save-peer/.test(cmd), 'save peer dependency'
+
+gt.test 'normal dependency command', ->
+  cmd = report [
+    [
+      name: 'lodash'
+      works: true
+      version: '0.3.0'
+    ]
+  ]
+  gt.ok /--save\ /.test(cmd), 'save normal dependency'
+
+gt.test 'dev dependency command', ->
+  cmd = report [
+    [
+      name: 'grunt'
+      works: true
+      version: '0.3.0'
+    ]
+  ]
+  gt.ok /--save-dev\ /.test(cmd), 'save dev dependency'
+
 gt.test 'usual input', ->
   cmd = report [
     [
@@ -50,6 +112,22 @@ gt.test 'usual input', ->
       version: 3
     ]
   ]
-  console.log cmd
   gt.ok /foo@3/.test(cmd), 'latest version is present'
   gt.ok !/foo@2/.test(cmd), 'previous version is not present'
+
+gt.test 'normal and dev dependency command', ->
+  cmd = report [
+    [
+      name: 'lodash'
+      works: true
+      version: '0.3.0'
+    ],
+    [
+      name: 'grunt'
+      works: true
+      version: '0.3.0'
+    ]
+  ]
+  console.log cmd
+  gt.ok /--save lodash@0\.3\.0/.test(cmd), 'normal dependency'
+  gt.ok /--save-dev grunt@0\.3\.0/.test(cmd), 'save dev dependency'
