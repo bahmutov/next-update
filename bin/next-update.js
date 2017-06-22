48d431bafb8487c930fca0a095c25fba63c5fe01
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-21 23:03:24 -0400

    lint bin js file

--- bin/next-update.js
+++ bin/next-update.js
@@ -1,39 +1,39 @@
 #!/usr/bin/env node
 
-var q = require('q');
-q.longStackSupport =  true;
+var q = require('q')
+q.longStackSupport = true
 
-var nextUpdate = require('../src/next-update');
-var program = require('../src/cli-options');
+var nextUpdate = require('../src/next-update')
+var program = require('../src/cli-options')
 
-var pkg = require('../package.json');
-var info = pkg.name + '@' + pkg.version + ' - ' + pkg.description;
+var pkg = require('../package.json')
+var info = pkg.name + '@' + pkg.version + ' - ' + pkg.description
 
-var report = require('../src/report').report;
-var revert = require('../src/revert');
+var report = require('../src/report').report
+var revert = require('../src/revert')
 
 if (program.available) {
   nextUpdate.available(program.module, {
     color: program.color
-  }).done();
+  }).done()
 } else if (program.revert) {
   revert(program.module)
     .then(function () {
-      console.log('done reverting');
+      console.log('done reverting')
     }, function (error) {
-      console.error('error while reverting\n', error);
-    });
+      console.error('error while reverting\n', error)
+    })
 } else {
   if (!program.tldr) {
-    console.log(info);
+    console.log(info)
   }
 
-  var updateNotifier = require('update-notifier');
+  var updateNotifier = require('update-notifier')
   updateNotifier({
     pkg: pkg,
     name: pkg.name,
     version: pkg.version
-  }).notify();
+  }).notify()
 
   var opts = {
     names: program.module,
@@ -45,30 +45,30 @@ if (program.available) {
     allow: program.allowed || program.allow,
     type: program.type,
     tldr: program.tldr
-  };
+  }
 
-  var checkCurrent = nextUpdate.checkCurrentInstall.bind(null, opts);
-  var checkCurrentState = program.skip ? q : checkCurrent;
-  var checkUpdates = nextUpdate.checkAllUpdates.bind(null, opts);
+  var checkCurrent = nextUpdate.checkCurrentInstall.bind(null, opts)
+  var checkCurrentState = program.skip ? q : checkCurrent
+  var checkUpdates = nextUpdate.checkAllUpdates.bind(null, opts)
 
-  var reportTestResults = function reportTestResults(results) {
+  var reportTestResults = function reportTestResults (results) {
     if (Array.isArray(results)) {
       return report(results, {
         useColors: program.color,
         keptUpdates: program.keep,
         changedLog: program['changed-log']
-      });
+      })
     }
-  };
+  }
 
   checkCurrentState()
     .then(checkUpdates)
     .then(reportTestResults)
     .catch(function (error) {
-      console.error('ERROR testing next working updates');
+      console.error('ERROR testing next working updates')
       if (error.stack) {
-        console.error(error.stack);
+        console.error(error.stack)
       }
-      process.exit(1);
-    });
+      process.exit(1)
+    })
 }
